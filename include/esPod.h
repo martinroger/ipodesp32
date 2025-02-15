144e74bca5b9c45a92b76c262e441f97527dc655
Martin <martin.roger@me.com>
2025-02-15 23:02:10 +0100

    Aggressive timings
    Seems to work with DCD pin pulled down.

--- include/esPod.h
+++ include/esPod.h
@@ -33,7 +33,7 @@
     #define RX_TASK_PRIORITY 2
 #endif
 #ifndef RX_TASK_INTERVAL_MS
-    #define RX_TASK_INTERVAL_MS 15
+    #define RX_TASK_INTERVAL_MS 10
 #endif
 //Process Task settings
 #ifndef PROCESS_TASK_STACK_SIZE
@@ -43,7 +43,7 @@
     #define PROCESS_TASK_PRIORITY 5
 #endif
 #ifndef PROCESS_INTERVAL_MS
-    #define PROCESS_INTERVAL_MS 20
+    #define PROCESS_INTERVAL_MS 15
 #endif
 //TX Task settings
 #ifndef TX_TASK_STACK_SIZE
@@ -53,7 +53,7 @@
     #define TX_TASK_PRIORITY 20
 #endif
 #ifndef TX_INTERVAL_MS
-    #define TX_INTERVAL_MS 90
+    #define TX_INTERVAL_MS 20
 #endif
 //General iPod settings
 #ifndef TOTAL_NUM_TRACKS
